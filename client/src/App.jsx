e76e67f54bbdeaaf2f1c4b2f290ed3e1725bbb73
rsaraswathy <saraswathy12096@gmail.com>
2024-04-12 14:38:09 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
